83f13c45d0f8a136ef251901f1914c9088d1077b
ViktorHajnal <viktorhajnal@gmail.com>
2023-11-07 16:59:22 +0100

    Final update 07.11.2023

--- gui/gui.py
+++ gui/gui.py
@@ -1,13 +1,15 @@
 import tkinter as tk
 from tkinter import ttk
 
+from fridge.fridge import SmartFridge
+
 class GUI:
     def __init__(self, root, groceries):
         self.root = root
+        self.groceries = groceries
         self.root.title("Smart Home")
         self.root.geometry("900x600")
-        self.create_tabs()
-        self.groceries = groceries
+        self.create_tabs()        
         
     #Kreianje koliko tabova nam treba
     def create_tabs(self):
@@ -23,6 +25,7 @@ class GUI:
 
         self.labels_fridge(self.tab1)
         self.labels_meteo(self.tab2)
+        
     #Kreiranje labela za overview tab SmartHome applikacije
     def labels_fridge(self, tab):
         
@@ -31,20 +34,20 @@ class GUI:
         groceries_label.grid(column=0, row=0, padx=5, pady=5, columnspan=2)
 
         # Create a text widget to display groceries
-        groceries_text = tk.Text(tab, height=10, width=40)
-        groceries_text.grid(column=0, row=1, padx=5, pady=5, columnspan=2)
-
+        self.groceries_text = tk.Text(tab, height=15, width=50)
+        self.groceries_text.grid(column=0, row=1, padx=5, pady=5, columnspan=2)
+        # Popunjavamo text field sa trenutnim podacima u bazi prilikom pokretanja programa
+        self.update_groceries_list()
+        
         # Create a button to add groceries
         add_button = ttk.Button(tab, text="Add Groceries", command=self.add_groceries)
         add_button.grid(column=0, row=2, padx=5, pady=5)
 
         # Create a button to remove groceries
         remove_button = ttk.Button(tab, text="Remove Groceries", command=self.remove_groceries)
-        remove_button.grid(column=1, row=2, padx=5, pady=5)
-        
+        remove_button.grid(column=1, row=2, padx=5, pady=5)        
 
-        self.groceries_text = groceries_text  # Store a reference to the text widget
-        self.update_groceries_list()
+               
         
     def add_groceries(self):
         self.entry_frame = tk.Frame(self.tab1, highlightbackground='black', highlightthickness=1)
@@ -71,7 +74,7 @@ class GUI:
         self.entry_grocery_date = tk.Entry(self.entry_frame, width=25)
         self.entry_grocery_date.grid(column=1, row=6, padx=5, pady=5)        
                        
-        submit_button = tk.Button(self.entry_frame, text='Submit', command=self.submit_add_remove_groceries)
+        submit_button = tk.Button(self.entry_frame, text='Submit', command=self.submit_add_groceries)
         submit_button.grid(column=0, row=7, padx=5, pady=5)
         
         close_button = tk.Button(self.entry_frame, text='Close', command=self.close_add_remove_groceries)
@@ -96,13 +99,26 @@ class GUI:
         self.entry_grocery_qty = tk.Entry(self.entry_frame, width=25)
         self.entry_grocery_qty.grid(column=1, row=5, padx=5, pady=5)     
                        
-        submit_button = tk.Button(self.entry_frame, text='Submit', command=self.submit_add_remove_groceries)
+        submit_button = tk.Button(self.entry_frame, text='Submit', command=self.submit_remove_groceries)
         submit_button.grid(column=0, row=7, padx=5, pady=5)
         
         close_button = tk.Button(self.entry_frame, text='Close', command=self.close_add_remove_groceries)
         close_button.grid(column=1, row=7, padx=5, pady=5)
             
-    def submit_add_remove_groceries(self):
+    def submit_add_groceries(self):
+        self.item_name = self.entry_grocery_name.get() 
+        self.item_quantity = abs(int(self.entry_grocery_qty.get()))
+        self.item_date = self.entry_grocery_date.get()
+        
+        self.entry_grocery_name.delete(0, tk.END)
+        self.entry_grocery_qty.delete(0, tk.END)
+        self.entry_grocery_date.delete(0, tk.END)
+        
+        self.update_groceries_list()
+        
+        return self.item_name, self.item_quantity, self.item_date
+    
+    def submit_remove_groceries(self):
         self.item_name = self.entry_grocery_name.get() 
         self.item_quantity = abs(int(self.entry_grocery_qty.get()))
         self.item_date = self.entry_grocery_date.get()
@@ -118,11 +134,12 @@ class GUI:
     def close_add_remove_groceries(self):
         self.entry_frame.destroy()
 
-    def update_groceries_list(self, self.groceries):
+    def update_groceries_list(self):
         
-        self.groceries_text.delete(0, tk.END)  # Clear the text widget           
+        self.groceries_text.delete(1.0, tk.END)  # Clear the text widget           
         for item in self.groceries:
             self.groceries_text.insert(tk.END, f"{item.name}: {item.quantity} (Expires on: {item.expiration_date}\n)")
+        
 
         
     #Kreiranje labela za Meteo tab SmartHome aplikacije
--- main.py
+++ main.py
@@ -8,9 +8,12 @@ def main():
     database_url = "sqlite:///smart_home.db"
     fridge = SmartFridge(database_url)    
     groceries_list = fridge.list_groceries()
+    for item in groceries_list:
+        print(f"{item.name}: {item.quantity} (Expires on: {item.expiration_date})")
     root = tk.Tk()
     GUI(root, groceries= groceries_list)
-      
+    
+  
     
     root.mainloop()
     fridge.close()
